460ea642c3eb2b0c484ed45858e06c5beda6cbce
Zandler Oliveira <zandler.oliveira@gmail.com>
2018-11-25 11:40:55 -0200

    criada fetura soma

--- calculadora.py
+++ calculadora.py
@@ -8,4 +8,8 @@ class Calculadora:
         print("Primeiro numero: {}".format(n1))
         print("Segundo numero: {}".format(n2))
     
+    def soma(n1, n2):
+        return n1 + n2
+
     retornaNumeros(n1,n2)
+    print("A soma e: {}".format(str(soma(n1, n2))))
