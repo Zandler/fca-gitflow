504870038a9fd62647fabac572c1b8074fbe88c2
Zandler Oliveira <zandler.oliveira@gmail.com>
2018-11-25 11:07:44 -0200

    arquivo inicial

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+# conding: utf-8
+
+class Calculadora:
+    n1= int(input("Digite o primeiro numero: "))
+    n2 = int(input("digite o segundo numero: "))
+
+    def retornaNumeros(n1, n2):
+        print("Primeiro numero: {}".format(n1))
+        print("Segundo numero: {}".format(n2))
+    
+    retornaNumeros(n1,n2)
